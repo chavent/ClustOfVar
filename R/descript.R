0579182a296b1cec251de1e42401cb8e7a306b3a
AmauryLabenne <amaury.labenne@irstea.fr>
2015-07-24 17:00:10 +0200

    modif pour avoir les sqload des quali dans ordre decroissant

--- R/descript.R
+++ R/descript.R
@@ -62,7 +62,8 @@ descript<-function(part,rec,matsim=FALSE)
       if (is.numeric(X[,clus[i]])) #ajout
         C[i,2]<-cor(X[,clus[i]], latent.var[,g], use="complete.obs")#ajout
     }
-    C<-C[order(abs(C[,2]),decreasing=TRUE), ] #ajout
+    #C<-C[order(abs(C[,2]),decreasing=TRUE), ] #ajout
+    C<-C[order(C[,1],decreasing=TRUE), ] #ajout
     var[[g]] <- C
     #coefficients of the score function
     beta <- matrix(NA,length(v)+1,1)
