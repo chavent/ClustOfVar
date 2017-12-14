04939ef11712747d490694f2cf218b88201a5219
Marie Chavent <marie.chavent@inria.fr>
2017-12-14 14:38:59 +0100

    bug print.clustab

--- R/print.clustab.R
+++ R/print.clustab.R
@@ -15,7 +15,7 @@ print.clustab <- function(x, ...){
   res <- matrix("",2,2)
   colnames(res) <-c("name","description")
   res[1,] <- c("$matCR", "matrix of corrected Rand indices")
-  res[2,] <- c("$meanR", "vector of mean corrected Rand indices")
+  res[2,] <- c("$meanCR", "vector of mean corrected Rand indices")
   row.names(res) <- rep("",2) 
   print(res)
 }
